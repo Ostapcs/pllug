501c65719a1f5411029f6f794d8e3f3c70a925bd
Ostap Kutyanskyy <ostapkutya@gmail.com>
2018-10-05 21:17:46 +0300

    first commit

--- Program.cs
+++ Program.cs
@@ -6,10 +6,55 @@ namespace pllug
 
     class MainClass
     {
+
+
+        public static bool isRightSize(Door door,Object obj)
+        {
+            if (obj.Height < door.Height && obj.Width < door.Width)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
         public static void Main(string[] args)
         {
-            Door a = new Door(2, 3);
+            System.Console.WriteLine("Enter the size of door :");
+
+            System.Console.WriteLine("Height : ");
+            string input  = System.Console.ReadLine();
+            double height = double.Parse(input);
+
+            System.Console.WriteLine("Width : ");
+            input = System.Console.ReadLine();
+            double width = double.Parse(input);
            
+            Door door = new Door(height,width);
+
+            System.Console.WriteLine("Enter the size of Object :");
+            System.Console.WriteLine("Height : ");
+            input = System.Console.ReadLine();
+            height = double.Parse(input);
+
+            System.Console.WriteLine("Width : ");
+            input = System.Console.ReadLine();
+            width = double.Parse(input);
+
+            Object obj = new Object(height, width);
+
+            if (isRightSize(door,obj))
+            {
+                System.Console.WriteLine("Everything is ok");
+            }
+            else
+            {
+                System.Console.WriteLine("Your Object is to big for your doors ");
+            }
+
         }
     }
 }
